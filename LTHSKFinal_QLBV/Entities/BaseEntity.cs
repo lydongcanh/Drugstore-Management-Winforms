4ecd7dadbafdab9bdfebd85fb34e11089f67b4f3
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-06-19 19:41:06 +0700

    Update BaseEntity. Add Equals, GetHashCode and CompareTo methods.

--- LTHSKFinal_QLBV/Entities/BaseEntity.cs
+++ LTHSKFinal_QLBV/Entities/BaseEntity.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace LTHSKFinal_QLBV
 {
-    public abstract class BaseEntity
+    public abstract class BaseEntity : IComparable<BaseEntity>
     {
         public abstract string EntityId { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            return EntityId.Equals(((BaseEntity)obj).EntityId);
+        }
+
+        public override int GetHashCode()
+        {
+            return EntityId.GetHashCode();
+        }
+
+        public int CompareTo(BaseEntity other)
+        {
+            return EntityId.CompareTo(other.EntityId);
+        }
     }
 }
