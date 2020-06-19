4d3cd38c8f618c4eda1aaced7b0221c2233935df
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-06-19 19:41:33 +0700

    Add BaseDAO.

---
+++ LTHSKFinal_QLBV/DAL/BaseDAO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTHSKFinal_QLBV.DAL
+{
+    public abstract class BaseDAO<T> where T: BaseEntity
+    {
+        private readonly LTHSKFinalDbDataContext dbContext = new LTHSKFinalDbDataContext();
+
+        public Table<T> Table
+        {
+            get { return dbContext.GetTable<T>(); }
+        }
+
+        public List<T> GetAll()
+        {
+            return Table.ToList();
+        }
+
+        public T GetById(string id)
+        {
+            return Table.Where(e => e.EntityId == id).FirstOrDefault();
+        }
+
+        public void Add(T entity)
+        {
+            if (Table.Where(e => e.Equals(entity)) != null)
+                throw new ArgumentException("Entity muốn thêm đã tồn tại.");
+
+            Table.InsertOnSubmit(entity);
+            dbContext.SubmitChanges();
+        }
+
+        public void Update(T entity)
+        {
+            var oldEntity = Table.FirstOrDefault(e => e.Equals(entity));
+            if (oldEntity == null)
+                throw new ArgumentException("Entity muốn cập nhật không tồn tại.");
+
+            oldEntity = entity;
+            dbContext.SubmitChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var entity = Table.FirstOrDefault(e => e.EntityId == id);
+            if (entity == null)
+                throw new ArgumentException("Entity muốn xóa không tồn tại.");
+
+            Table.DeleteOnSubmit(entity);
+            dbContext.SubmitChanges();
+        }
+
+        public int Count()
+        {
+            return Table.Count();
+        }
+    }
+}
